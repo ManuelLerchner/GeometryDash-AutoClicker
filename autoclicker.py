330a2b4e6fd2f7fbd2d46fbcb082bd19b768a859
Tobias Eppacher <tobi.eppacher@gmail.com>
2021-09-09 16:46:07 +0200

    Co-authored-by: ManuelLerchner <ManuelLerchner@users.noreply.github.com>

--- autoclicker.py
+++ autoclicker.py
@@ -1,3 +1,20 @@
-import pynput
-import time
+from pynput import keyboard
+from pynput import mouse
 
+
+def on_press(key):
+    print(key)
+
+
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+
+def wait_for_user_input():
+    listener = keyboard.Listener(on_press=on_press, on_release=on_release)
+    listener.start()
+    listener.join()  
+    
+wait_for_user_input()

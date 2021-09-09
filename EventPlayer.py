4cd0b6bd66d791d93af36bd4480690355b47380e
Tobias Eppacher <tobi.eppacher@gmail.com>
2021-09-09 22:15:36 +0200

    Added Play-Function to EventPlayer

--- EventPlayer.py
+++ EventPlayer.py
@@ -1,6 +1,7 @@
 from pynput.mouse import Button, Controller
 from pynput import keyboard
-from time import time
+import time
+
 
 class EventPlayer:
 
@@ -11,26 +12,40 @@ class EventPlayer:
     def getEvents(self, fileName):
         with open(f"savedRecordings/{fileName}.txt", 'r') as data:
             for line in data:
-                event = line.strip()[1:-1].replace(' ', '').replace("'", '').split(",")    
+                event = line.strip()[1:-1].replace(' ',
+                                                   '').replace("'", '').split(",")
                 self.eventList.append(event)
 
+    def playFile(self):
+        startTime = time.time()
+        print(startTime)
+        currentIndex = 0
+        while currentIndex < len(self.eventList):
+            if(time.time()-startTime >= float(self.eventList[currentIndex][0])):
+
+                # Execute Event with Index "currentIndex"
+                print(currentIndex, self.eventList[currentIndex])
+                self.executeEvent()
+                currentIndex += 1
+
     def executeEvent(self, time, eventGroup, eventType, id):
-        if(eventGroup=="KEY"):
+
+        if(eventGroup == "KEY"):
             pass
 
-        if(eventGroup=="MOUSE"):
-
-            if eventType=="PRESS":
+        if(eventGroup == "MOUSE"):
+            if eventType == "PRESS":
                 self.mouse.press(button)
-            
-            if eventType=="RELEASE":
+
+            if eventType == "RELEASE":
                 self.mouse.release(button)
 
     def printEventList(self):
         for event in self.eventList:
             print(event)
 
-EP=EventPlayer()
+
+EP = EventPlayer()
 
 EP.getEvents("Test123")
 EP.printEventList()
